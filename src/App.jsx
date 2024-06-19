146840b12bf41416a55ebb225c2bbaa782952bea
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-06-19 11:53:09 +0600

    recharts added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar';
 import PriceOptions from './Components/PriceOptions/PriceOptions';
+import LineChart from './Components/LineChart/LineChart';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className='pb-12'>
       <Navbar></Navbar>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </div>
   )
 }
---
+++ src/Components/LineChart/LineChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { LineChart as LChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const LineChart = () => {
+  const marksData = [
+    { subject: 'Math', marks: 85 },
+    { subject: 'Science', marks: 90 },
+    { subject: 'History', marks: 78 },
+    { subject: 'English', marks: 92 },
+    { subject: 'Geography', marks: 88 },
+  ];
+
+  return (
+    <div className='mt-20'>
+      <div className='w-11/12 h-96 mx-auto'>
+        <ResponsiveContainer width="100%" height="100%">
+          <LChart data={marksData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="subject" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="marks" stroke="#8884d8" />
+          </LChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default LineChart;
